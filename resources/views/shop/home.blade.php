3400b876d0306a51e851d2ed1a6009c1914fd7f9
Emiliano De León <114441890+MojoPin50Y8b@users.noreply.github.com>
2025-08-25 08:54:16 -0600

    Routes definido

---
+++ resources/views/shop/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app') {{-- o tu layout principal --}}
+@section('content')
+    <div class="p-8">
+        <h1 class="text-2xl font-bold">Home (Shop)</h1>
+        <p>Placeholder de la tienda.</p>
+    </div>
+@endsection
